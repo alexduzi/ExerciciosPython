12a2f162468fba8a46ae20c10a200244ea6e2ca0
Duzi <Duzi>
2015-02-09 17:40:30 -0200

    Adição da classe

---
+++ ExerciciosPython_DNATEC/ExerciciosPython_DNATEC/Exercicios.py
@@ -0,0 +1,26 @@
+# 1. Escreva uma função que mapeia uma uma lista de palavras numa lista de inteiros que representam o tamanho de cada palavra. 
+# Escreva essa função de 4 formas: 
+# a. Usando um loop for 
+# b. Usando a função map 
+# c. Usando list comprehensions 
+# d. Usando dict comprehensions 
+# 2. Escreva uma função que recebe o nome de um filme e escreve na uma frase como esta: 
+# "Meu filme favorito é La Belle Verte". Essa função deve ter o valor default do nome do filme "La Belle Verte". 
+# 3. Escreva uma função de 'correção ortográfica' simplificada que recebe uma string e: 
+# a. Se houver mais de um espaço entre as palavras, substitua-o por um. 
+# b. Insere um espaço depois do ponto se o ponto estiver justaposto a palavra Ex: corrige("Frase a ser corrida.Outra frase.") -> 
+# "Frase a ser corrigida. Outra Frase" 
+# 4. Defina uma classe Circulo que pode ser construída a partir do seu raio. 
+# Essa classe deve possuir um método para calcular a sua área. 
+# 5. Escreva uma classe Polígono e duas classes derivadas, Retângulo e Triângulo que podem ser construidas passando largura e altura. 
+# Novamente as classes devem possuir um método para calcular a área. 
+# 6. Escreva uma função que recebe uma string com uma fita de DNA Ex "GATTACA" e retorna a fita complementar: "CTAATGT" 
+# 7. Escreva uma função que calcula o enésmo termo da sequência de Fibonacci (Fn = F(n-1) + F(n-2). 
+# 8. Escreva uma função que adiciona 2 números. a. Escreva uma função que recebe N números como parâmetros e retorna a soma. 
+# 9. Escreva uma função que recebe um número arbitrário de parâmetros de palavra chave e escreve na tela uma 
+# linha "parâmetro = valor" para cara parâmetro passado. 
+# 10. Use a biblioteca Turtle e escreva uma função recursiva que desenha um fractal.
+
+
+def __init__():
+    print "Hello World!"
